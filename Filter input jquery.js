94a602737cc11829f7be6147567147011448aa94
sebastian <sebaprogramer@gmail.com>
2020-11-28 11:26:31 -0300

    ultimos ejercicios

---
+++ Filter input jquery.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("#myInput").on("keyup", function() {
+      var value = $(this).val().toLowerCase();
+      $("#contenido tr").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+    });
